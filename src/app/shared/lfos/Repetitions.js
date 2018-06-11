c8e9adcd3adb930ca5dfcd048ee79ec9f2d11022
Adrien Husson <husson.adr@gmail.com>
2018-06-11 17:58:59 +0200

    fix repetition

--- src/app/shared/lfos/Repetitions.js
+++ src/app/shared/lfos/Repetitions.js
@@ -39,11 +39,13 @@ class Repetitions extends BaseLfo {
     this.energy = 0;
     this.energyBuffer = [];
     this.energyBufferLength = 50;
+    this.energyBufferIndex = 0;
+
     this.precisionThreshold = 5;
     this.lastSentEnergyValue = 1e9;
-    this.filteredEnergyBuffer = [];
-    this.filteredEnergyBufferLength = 50;
-    this.filteredEnergyBufferIndex = 0;
+    // this.filteredEnergyBuffer = [];
+    // this.filteredEnergyBufferLength = 50;
+    // this.filteredEnergyBufferIndex = 0;
     this.min = 1e9;
     this.max = -1e9;
     this.sum = 0; // not used
@@ -53,9 +55,9 @@ class Repetitions extends BaseLfo {
       this.energyBuffer.push(0);
     }
 
-    for (let i = 0; i < this.filteredEnergyBufferLength; i++) {
-      this.filteredEnergyBuffer.push(0);
-    }
+    // for (let i = 0; i < this.filteredEnergyBufferLength; i++) {
+    //   this.filteredEnergyBuffer.push(0);
+    // }
   }
 
   processStreamParams(prevStreamParams) {
@@ -83,84 +85,94 @@ class Repetitions extends BaseLfo {
     // transcribed from patch preprocessing (energy) :
 
     const d = frame.data;
+    //this.energy = 100 * Math.sqrt(d[0] * d[0] + d[1] * d[1] + d[2] * d[2]);
     this.energy = 100 * Math.sqrt(d[0] * d[0] + d[1] * d[1] + d[2] * d[2]);
 
     // this goes to bg coloured slider
 
     // transcribed from DynamicRange.js
 
-    this.energyBuffer.push(this.energy);
-    const first = this.energyBuffer.shift();
+    // this.energyBuffer.push(this.energy);
+    // const first = this.energyBuffer.shift();
     // const last = this.energy;
 
     // this.sum += last
     // this.sum -= first;
     // this.mean = this.sum / this.energyBufferLength;
 
-    let min = 1e9; // huuuuge positive
-    let max = -1e9; // huuuuuuuuge negative
+    // let min = 1e9; // huuuuge positive
+    // let max = -1e9; // huuuuuuuuge negative
 
-    for (var i = 0; i < this.energyBuffer.length; i++) {
-      if (this.energyBuffer[i] < min) {
-        min = this.energyBuffer[i];
-      }
+    // for (var i = 0; i < this.energyBuffer.length; i++) {
+    //   if (this.energyBuffer[i] < min) {
+    //     min = this.energyBuffer[i];
+    //   }
 
-      if (this.energyBuffer[i] > max) {
-        max = this.energyBuffer[i];
-      }
-    }
+    //   if (this.energyBuffer[i] > max) {
+    //     max = this.energyBuffer[i];
+    //   }
+    // }
 
-    let range = max - min;
+    // let range = max - min;
 
     // transcribed from patch intermediary processing
 
-    range *= 0.1; // arbitrary factor
+    // range *= 0.01; // arbitrary factor
 
-    if (range > 1) {
-      this.precisionThreshold = range >= 0 ? range : this.precisionThreshold;
-    }
+    // if (range > 1) {
+    //   this.precisionThreshold = range >= 0 ? range : this.precisionThreshold;
+    // }
 
     // transcribed from DataPrecisionFilter
 
     let trig = 0; // don't trig, 1: up trig, 2: down trig
 
-    if (Math.abs(this.energy - this.lastSentEnergyValue) > this.precisionThreshold) {
-      // this.lastSentEnergyValue = this.energy;
+    if (Math.abs(this.energy - this.lastSentEnergyValue) < this.precisionThreshold) {
+      this.energy = this.lastSentEnergyValue;
+    }
 
-      // then do the stuff and propagate frames :
-      // transcribed from StepDetection
 
-      if (this.filteredEnergyBufferIndex < this.filteredEnergyBufferLength) {
-        this.filteredEnergyBuffer[this.filteredEnergyBufferIndex] = this.energy;
-        this.filteredEnergyBufferIndex++;
-      } else {
-        this.filteredEnergyBufferIndex = 0;
-        this.min = 1e9;
-        this.max = -1e9;
+    // this.ltSentEnergyValue = this.energy;
 
-        for (let i = 0; i < this.filteredEnergyBufferLength; i++) {
-          if (this.filteredEnergyBuffer[i] < min) {
-            this.min = this.filteredEnergyBuffer[i];
-          }
+    // then do the stuff and propagate frames :
+    // transcribed from StepDetection
 
-          if (this.filteredEnergyBuffer[i] > max) {
-            this.max = this.filteredEnergyBuffer[i];
-          }
+    if (this.energyBufferIndex < this.energyBufferLength) {
+      this.energyBuffer[this.energyBufferIndex] = this.energy;
+      this.energyBufferIndex++;
+
+      // this.energyBuffer.push(this.energy);
+      // const first = this.energyBuffer.shift();
+      // this.filteredEnergyBuffer[this.filteredEnergyBufferIndex] = this.energy;
+      // this.filteredEnergyBufferIndex++;
+    } else {
+      //this.filteredEnergyBufferIndex = 0;
+      this.min = 1e9;
+      this.max = -1e9;
+
+      for (let i = 0; i < this.energyBufferLength; i++) {
+        if (this.energyBuffer[i] < this.min) {
+          this.min = this.energyBuffer[i];
+        }
+
+        if (this.energyBuffer[i] > this.max) {
+          this.max = this.energyBuffer[i];
         }
       }
-
-      this.mean = (this.max + this.min) * 0.5;
-
-      if (this.energy >= this.mean && this.lastSentEnergyValue < this.mean) {
-        trig = 1;
-      }
-
-      if (this.energy <= this.mean && this.lastSentEnergyValue > this.mean) {
-        trig = 2;
-      }
-
-      this.lastSentEnergyValue = this.energy;
+      this.energyBufferIndex = 0;
     }
+
+    this.mean = (this.max + this.min) * 0.5;
+
+    if (this.energy >= this.mean && this.lastSentEnergyValue < this.mean) {
+      trig = 1;
+    }
+
+    if (this.energy <= this.mean && this.lastSentEnergyValue > this.mean) {
+      trig = 2;
+    }
+
+    this.lastSentEnergyValue = this.energy;
 
     this.frame.time = frame.time;
 
@@ -168,8 +180,8 @@ class Repetitions extends BaseLfo {
     this.frame.data[1] = this.mean;
     this.frame.data[2] = trig;
 
-    this.frame.metadata = frame.metadata;
+    this.frame.metadasata = frame.metadata;
   }
 };
 
-export default Repetitions;
+ export default Repetitions;
